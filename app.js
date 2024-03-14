3a1fe8fe8e81f82a3a2fb60ea29933798ac7685e
Endriw Costa <endriw.costa3@gmail.com>
2024-03-14 18:50:58 -0300

    Loops e tentativas

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 3;
+let numeroSecreto = 5;
 console.log('O número secreto é o', numeroSecreto);
-let chute = prompt('Escolha um número entre 1 a 10');
+let chute;
+let tentativas = 1;
 
-// Se o chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso aí! Você descobriu o número secreto (${numeroSecreto})`);
-} else {
-    alert('Você errou! Tente novamente.');
+// emquanto chute não for igual ao numeroSecreto
+while (chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 a 10');
+    // Se o chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número é menor que ${chute}`);
+        } else {
+            alert(`O número é maior que ${chute}`);
+        }
+        
+    }
+    // tentativas = tentativas + 1
+    tentativas++;
 }
