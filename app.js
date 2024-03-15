3510167eed90cac98b81bfbd7dcb833129cd1d0a
Endriw Costa <endriw.costa3@gmail.com>
2024-03-15 19:38:13 -0300

    Boas praticas em Programação

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log('O número secreto é o', numeroSecreto);
 let chute;
 let tentativas = 1;
 
 // emquanto chute não for igual ao numeroSecreto
 while (chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 1 a 10');
+    chute = prompt('Escolha um número entre 1 a 100');
     // Se o chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+       break;
     } else {
         if (chute > numeroSecreto) {
             alert(`O número é menor que ${chute}`);
@@ -21,3 +21,13 @@ while (chute != numeroSecreto){
     // tentativas = tentativas + 1
     tentativas++;
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+// em vez de fazer isso abaixo
+// if (tentativas > 1){
+//     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+// }else {
+//     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+// }
+
